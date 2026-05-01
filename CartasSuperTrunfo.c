e0b8841a51c8f8efb8d5bb2aab805956d463288f
NerfLuan <luan.uny3@gmail.com>
2026-05-01 21:37:58 +0000

    Adicionei as duas carta.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,29 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
+  // Carta 1
+ 
+  char estado1;
+  char codigo1[4];
+  char cidade1[19];
+  char cidade1[50];
+  int populaçao1;
+  float area1;
+  float pib1;
+  int pontos;
+
+// Carta 2
+
+char estado2;
+char codigo2[17];
+char cidade2[60];
+int população1;
+float area2;
+float pib2;
+int pontos2;
+
   // Área para entrada de dados
+  
 
   // Área para exibição dos dados da cidade
 
